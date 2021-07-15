fd58f68233ceea34a6cb3863667c1c587538ad02
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-07-15 18:07:08 +0530

    Refactor:Changes made in test method

--- src/test/java/com/bridgelabz/MaximumTest.java
+++ src/test/java/com/bridgelabz/MaximumTest.java
@@ -24,4 +24,13 @@ public class MaximumTest {
         String result = Maximum.GenMaximum("Apple","Peach","Banana");
         Assert.assertEquals(java.util.Optional.of("Peach"),java.util.Optional.of(result));
     }
+
+    //...Generic Class...//
+
+    @Test
+    public void testMethod_for_Generic_Class() {
+        Assert.assertEquals(java.util.Optional.of(3),java.util.Optional.of(Maximum.GenMaximumTow(1,3,2)));
+        Assert.assertEquals(java.util.Optional.of(3.3f),java.util.Optional.of(Maximum.GenMaximumTow(1.1f,2.2f,3.3f)));
+        Assert.assertEquals(java.util.Optional.of("Peach"),java.util.Optional.of(Maximum.GenMaximumTow("Peach","Banana","Apple")));
+    }
 }
